6f231108a220b534012ef76650f87ca934debf22
Aron Polner <aron.polner@gmail.com>
2019-10-18 10:42:57 +0200

    The mesh is now one chunk

--- Assets/Delaunay/DelaunayMesh.cs
+++ Assets/Delaunay/DelaunayMesh.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using TriangleNet.Geometry;
 using TriangleNet.Topology;
 
-public class DelaunayMesh : MonoBehaviour {
+public class DelaunayMesh : MonoBehaviour
+{
+
+    [SerializeField] private int trianglesInChunk = 5000;
 
     // Prefab which is generated for each chunk of the mesh.
     public Transform chunkPrefab = null;
@@ -53,64 +56,101 @@ public class DelaunayMesh : MonoBehaviour {
         if (chunks != null)
         {
             foreach (Transform chunk in chunks)
-            Destroy(chunk.gameObject);
+                Destroy(chunk.gameObject);
         }
         chunks.Clear();
 
         MakeMesh();
     }
-    
-    public void MakeMesh() {
+
+    public void MakeMesh()
+    {
+        // Instantiate an enumerator to go over the Triangle.Net triangles - they don't
+        // provide any array-like interface for indexing
         IEnumerator<Triangle> triangleEnumerator = mesh.Triangles.GetEnumerator();
 
-        for (int chunkStart = 0; chunkStart < mesh.Triangles.Count; chunkStart++) {
+        var numberOfChunks = 1;
+        // Create more than one chunk, if necessary
+        for (int chunkStart = 0; chunkStart < mesh.Triangles.Count; chunkStart += trianglesInChunk)
+        {
+            Debug.Log($"Chunk {numberOfChunks++}!");
+            // Vertices in the unity mesh
             List<Vector3> vertices = new List<Vector3>();
+
+            // Per-vertex normals
             List<Vector3> normals = new List<Vector3>();
+
+            // Per-vertex UVs - unused here, but Unity still wants them
             List<Vector2> uvs = new List<Vector2>();
+
+            // Triangles - each triangle is made of three indices in the vertices array
             List<int> triangles = new List<int>();
 
-            if (!triangleEnumerator.MoveNext())
+            // Iterate over all the triangles until we hit the maximum chunk size
+            int chunkEnd = chunkStart + trianglesInChunk;
+            for (int i = chunkStart; i < chunkEnd; i++)
             {
-                break;
+                if (!triangleEnumerator.MoveNext())
+                {
+                    // If we hit the last triangle before we hit the end of the chunk, stop
+                    break;
+                }
+
+                // Get the current triangle
+                Triangle triangle = triangleEnumerator.Current;
+
+                // For the triangles to be right-side up, they need
+                // to be wound in the opposite direction
+                Vector3 v0 = GetPoint3D(triangle.vertices[2].id);
+                Vector3 v1 = GetPoint3D(triangle.vertices[1].id);
+                Vector3 v2 = GetPoint3D(triangle.vertices[0].id);
+
+                // This triangle is made of the next three vertices to be added
+                triangles.Add(vertices.Count);
+                triangles.Add(vertices.Count + 1);
+                triangles.Add(vertices.Count + 2);
+
+                // Add the vertices
+                vertices.Add(v0);
+                vertices.Add(v1);
+                vertices.Add(v2);
+
+                // Compute the normal - flat shaded, so the vertices all have the same normal
+                Vector3 normal = Vector3.Cross(v1 - v0, v2 - v0);
+                normals.Add(normal);
+                normals.Add(normal);
+                normals.Add(normal);
+
+                // If you want to texture your terrain, UVs are important,
+                // but I just use a flat color so put in dummy coords
+                uvs.Add(new Vector2(0.0f, 0.0f));
+                uvs.Add(new Vector2(0.0f, 0.0f));
+                uvs.Add(new Vector2(0.0f, 0.0f));
             }
 
-            Triangle triangle = triangleEnumerator.Current;
-
-            // For the triangles to be right-side up, they need
-            // to be wound in the opposite direction
-            Vector3 v0 = GetPoint3D(triangle.vertices[2].id);
-            Vector3 v1 = GetPoint3D(triangle.vertices[1].id);
-            Vector3 v2 = GetPoint3D(triangle.vertices[0].id);
-
-            triangles.Add(vertices.Count);
-            triangles.Add(vertices.Count + 1);
-            triangles.Add(vertices.Count + 2);
-
-            vertices.Add(v0);
-            vertices.Add(v1);
-            vertices.Add(v2);
-
-            Vector3 normal = Vector3.Cross(v1 - v0, v2 - v0);
-            normals.Add(normal);
-            normals.Add(normal);
-            normals.Add(normal);
-
-            uvs.Add(new Vector2(0.0f, 0.0f));
-            uvs.Add(new Vector2(0.0f, 0.0f));
-            uvs.Add(new Vector2(0.0f, 0.0f));
-
+            // Create the actual Unity mesh object
             Mesh chunkMesh = new Mesh();
             chunkMesh.vertices = vertices.ToArray();
             chunkMesh.uv = uvs.ToArray();
             chunkMesh.triangles = triangles.ToArray();
             chunkMesh.normals = normals.ToArray();
 
+            // Instantiate the GameObject which will display this chunk
             Transform chunk = Instantiate<Transform>(chunkPrefab, transform.position, transform.rotation);
             chunk.GetComponent<MeshFilter>().mesh = chunkMesh;
             chunk.GetComponent<MeshCollider>().sharedMesh = chunkMesh;
             chunk.transform.parent = transform;
-            chunks.Add(chunk);
         }
+    }
+
+    private void ClearMesh()
+    {
+        if (chunks != null)
+        {
+            foreach (Transform chunk in chunks)
+                Destroy(chunk.gameObject);
+        }
+        chunks.Clear();
     }
 
     public bool MeshVisible
@@ -126,20 +166,24 @@ public class DelaunayMesh : MonoBehaviour {
     }
 
     /* Returns a point's local coordinates. */
-    public Vector3 GetPoint3D(int index) {
+    public Vector3 GetPoint3D(int index)
+    {
         Vertex vertex = mesh.vertices[index];
         float elevation = elevations[index];
         return new Vector3((float)vertex.x, elevation, (float)vertex.y);
     }
-    
-    public void OnDrawGizmos() {
-        if (mesh == null) {
+
+    public void OnDrawGizmos()
+    {
+        if (mesh == null)
+        {
             // Probably in the editor
             return;
         }
 
         Gizmos.color = Color.red;
-        foreach (Edge edge in mesh.Edges) {
+        foreach (Edge edge in mesh.Edges)
+        {
             Vertex v0 = mesh.vertices[edge.P0];
             Vertex v1 = mesh.vertices[edge.P1];
             Vector3 p0 = new Vector3((float)v0.x, 0.0f, (float)v0.y);
