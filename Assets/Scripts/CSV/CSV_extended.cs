829396a4c3c3457781130e9eaf89f5351be64a19
Aron Polner <aron.polner@gmail.com>
2019-10-22 10:44:20 +0200

    Added code that parses the new csv data

---
+++ Assets/Scripts/CSV/CSV_extended.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Thoughts: does this even need to be a monobehaviour?
+ * 
+ */
+
+public class CSV_extended : MonoBehaviour
+{
+    [SerializeField] private string pathToFile;
+
+    private List<float[]> parsedData = new List<float[]>(); 
+
+    private void Start()
+    {
+        ParseCsvFileUsingResources(pathToFile);
+    }
+
+    public void ParseCsvFileUsingResources(string pathToFile)
+    {
+        TextAsset file = Resources.Load(pathToFile) as TextAsset;
+        string[] lines = file.text.Split('\n');
+
+        foreach(string line in lines)
+        {
+            string[] split = line.Split(',');
+
+            float[] parsedLine = new float[]
+            {
+                float.Parse(split[0]),
+                float.Parse(split[1]),
+                float.Parse(split[2]),
+                float.Parse(split[3]),
+                float.Parse(split[4]),
+                float.Parse(split[5]),
+                float.Parse(split[6])
+            };
+            parsedData.Add(parsedLine);
+        }
+    }
+}
