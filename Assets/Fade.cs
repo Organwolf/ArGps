affd36f6a346c316eda835509ef6cd1a723a9fd2
Aron Polner <aron.polner@gmail.com>
2019-11-01 15:17:23 +0100

    Added bounce to the InstructionsPanel
    Bool parameter "show" added and toggle logic created in  the Fade.cs script

---
+++ Assets/Fade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+
+    public bool show;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (show) show = false;
+            else show = true;
+            GetComponentInChildren<Animator>().SetBool("show", show);
+        }
+    }
+}
